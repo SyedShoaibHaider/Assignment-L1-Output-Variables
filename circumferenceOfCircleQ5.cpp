1bd33b6a7d66c4906f7b786596dd82b070615367
Shoaib Haider <128279754+SyedShoaibHaider@users.noreply.github.com>
2024-05-07 23:03:38 +0530

    assignment  1 solved

---
+++ circumferenceOfCircleQ5.cpp
@@ -0,0 +1,11 @@
+//Ques 5: WAP to find the circumference of a circle with radius 10 in C++.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r = 10;
+    float circum = 2*3.14*r;
+    cout<<circum;
+    return 0;
+}
